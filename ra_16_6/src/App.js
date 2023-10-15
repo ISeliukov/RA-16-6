d5fe42902d3a394b4623165d87eee73a17d75974
Garysi <garysi@rambler.ru>
2023-10-15 14:49:33 +0300

    add ver 0.1

---
+++ ra_16_6/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import shortId from 'shortid';
+import { Clock } from './engine/Clock';
+import { Form } from './engine/Form';
+import './App.css';
+
+const copyArrayOfObjects = (arr) => arr.map(obj => {
+  return {...obj};
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clocks: [
+        {name: 'Minsk', timeZone: '3', id: shortId.generate(), },
+        {name: 'New York', timeZone: '-4', id: shortId.generate(), },
+        {name: 'Paris', timeZone: '1', id: shortId.generate(), }
+      ],
+    }
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleAdd(e, name, timeZone) {
+    e.preventDefault();
+    this.setState({
+      clocks: [
+        ...copyArrayOfObjects(this.state.clocks), 
+        {
+          name: name, 
+          timeZone: timeZone, 
+          id: shortId.generate()
+        }
+      ]});
+  }
+
+  handleDelete(id) {
+    this.setState(prevState => {
+      const newClocks = prevState.clocks.filter(clock => clock.id !== id);
+      return {
+        clocks: newClocks
+      }
+    })
+  }
+
+  render() {
+    const {clocks} = this.state;
+    return (
+      <div className="App">
+        <Form handleAdd={this.handleAdd}/>
+        <div className="clocks-wrapper">
+          {
+            clocks.map(clock => <Clock key={clock.id} clock={clock} handleDelete={this.handleDelete}/>)
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export { App };
---
+++ ra_16_6/src/engine/Clock.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      rafId: null,
+    }
+    this.repeatOften = this.repeatOften.bind(this);
+  }
+
+  repeatOften() {
+    const clock = this.props.clock;
+    const offset = parseInt(clock.timeZone);
+
+    this.setState({
+      hours: moment().utcOffset(offset).hours(),
+      minutes: moment().utcOffset(offset).minutes(),
+      seconds: moment().utcOffset(offset).seconds(),
+      rafId: requestAnimationFrame(this.repeatOften),
+    })
+  }
+
+  componentDidMount() {
+    requestAnimationFrame(this.repeatOften);
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.state.rafId);
+  }
+
+  render() {
+    const {hours, minutes, seconds} = this.state;
+    const {handleDelete, clock} = this.props;
+
+    return (
+      <div className="clock-wrapper">
+        <span className="clock-name">{clock.name}</span>
+        <div className="clock-watches">
+          <div className="clock-hand clock-hand__hours" style={{transform: `rotate(${hours * 360 / 12}deg)`}}></div>
+          <div className="clock-hand" style={{transform: `rotate(${minutes * 360 / 60}deg)`}}></div>
+          <div className="clock-hand clock-hand__seconds" style={{transform: `rotate(${seconds * 360 / 60}deg)`}}></div>
+          <button className="clock-btn" onClick={() => handleDelete(clock.id)} aria-label="delete clock">x</button>
+        </div>
+        <span className="clock-digital">{`${hours}:${minutes}:${seconds}`}</span>
+      </div>
+    );
+  }
+}
+
+export { Clock };
